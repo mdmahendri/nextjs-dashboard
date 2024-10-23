e5ed3d26ac99b996c045bbbfe0fc2c978e5c6e1c
mdmahendri <mahendri.md@gmail.com>
2024-10-24 02:51:46 +0700

    preload requires font subsets declared

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,6 @@
 import { Inter, Lusitana } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
-const lusitana = Lusitana({weight: "400"})
+const lusitana = Lusitana({ subsets: ['latin'], weight: "400"})
 
 export { inter, lusitana }
